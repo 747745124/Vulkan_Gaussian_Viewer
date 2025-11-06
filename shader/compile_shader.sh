aece741a2cec0d421b7ef23aa17bc7f1d3b40b76
Naoyuki <zhaonzh@gmail.com>
2025-11-05 18:39:47 -0800

    shell

--- shader/compile_shader.sh
+++ shader/compile_shader.sh
@@ -1,6 +1,2 @@
-glslc ./shader/triangle.vert -o ./shader/triangle.vert.spv
-glslc ./shader/triangle.frag -o ./shader/triangle.frag.spv
-glslc ./shader/point.vert -o ./shader/point.vert.spv
-glslc ./shader/point.frag -o ./shader/point.frag.spv
 glslc ./shader/splat.vert -o ./shader/splat.vert.spv
 glslc ./shader/splat.frag -o ./shader/splat.frag.spv
